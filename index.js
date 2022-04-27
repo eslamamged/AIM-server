2b84cfffe06516133af51455b03d53054b07240b
eslamamged <solomamged72@gmail.com>
2022-04-27 21:50:48 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const app = express();
+app.use(express.json());
+app.use(cors());
+const recipeRouter = require("./Routes/recipeRouter");
+app.use(express.urlencoded({ extended: true }));
+app.use("/recipes", recipeRouter);
+app.use("/public", express.static(__dirname + "/public"));
+
+// this is wildcard API to handle the wrong url
+app.get("*", (req, res, next) => {
+  const err = new Error("sorry,this is invalid url");
+  err.status = "fail";
+  err.statusCode = "404";
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "fail";
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    errors: err.errors || [],
+  });
+});
+
+app.use((err, req, res, next) => {
+  if (
+    err.name === "MongoError" ||
+    err.name === "ValidationError" ||
+    err.name === "CastError"
+  ) {
+    err.status = 422;
+  }
+  if (req.get("accept").includes("json")) {
+    res
+      .status(err.status || 500)
+      .json({ message: err.message || "some error eccured. " });
+  } else {
+    res
+      .status(err.status || 500)
+      .sendFile(path.join(_dirname, "public", "index.html"));
+  }
+});
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,17 @@
+const app = require("./index.js");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    app.listen(port, () => {
+      console.log(`this app is listening on port ${port}....`);
+    });
+  })
+  .catch((err) => console.log(err));
